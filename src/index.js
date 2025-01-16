6c3947c03dd134738015b1456703ce6b375dfb7c
ToxaDex <xxxwalken09@gmail.com>
2025-01-16 14:33:30 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Entry point for the ToxaDex SDK.
+const wallet = require("./wallet");
+const transactions = require("./transactions");
+const analytics = require("./analytics");
+
+module.exports = {
+  wallet,
+  transactions,
+  analytics,
+};
